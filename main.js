2ed7cc6240b411afd0bfd852eedcd52855705500
AkkuRam <reachakhil10@gmail.com>
2023-06-17 19:39:16 +0200

    confirmation messages to completing tasks (TODO)

---
+++ main.js
@@ -0,0 +1,78 @@
+function addFrance() {
+    // Get the displayFrance div
+    var displayDiv = document.getElementById("displayFrance");
+
+    // Update the content of the displayDiv
+    displayDiv.innerHTML = "France";
+
+    // Show the displayDiv
+    displayDiv.style.display = "block";
+}
+
+function addAustralia() {
+    // Get the displayFrance div
+    var displayDiv = document.getElementById("displayAustralia");
+
+    // Update the content of the displayDiv
+    displayDiv.innerHTML = "Australia";
+
+    // Show the displayDiv
+    displayDiv.style.display = "block";
+}
+
+function addChina() {
+    // Get the displayFrance div
+    var displayDiv = document.getElementById("displayChina");
+
+    // Update the content of the displayDiv
+    displayDiv.innerHTML = "China";
+
+    // Show the displayDiv
+    displayDiv.style.display = "block";
+}
+
+function addSouthSudan() {
+    // Get the displayFrance div
+    var displayDiv = document.getElementById("displaySouthSudan");
+
+    // Update the content of the displayDiv
+    displayDiv.innerHTML = "South Sudan";
+
+    // Show the displayDiv
+    displayDiv.style.display = "block";
+}
+
+function addSweden() {
+    // Get the displayFrance div
+    var displayDiv = document.getElementById("displaySweden");
+
+    // Update the content of the displayDiv
+    displayDiv.innerHTML = "Sweden";
+
+    // Show the displayDiv
+    displayDiv.style.display = "block";
+}
+
+
+function addBrazil() {
+    // Get the displayFrance div
+    var displayDiv = document.getElementById("displayBrazil");
+
+    // Update the content of the displayDiv
+    displayDiv.innerHTML = "Brazil";
+
+    // Show the displayDiv
+    displayDiv.style.display = "block";
+}
+
+
+function addUSA() {
+    // Get the displayFrance div
+    var displayDiv = document.getElementById("displayUSA");
+
+    // Update the content of the displayDiv
+    displayDiv.innerHTML = "USA";
+
+    // Show the displayDiv
+    displayDiv.style.display = "block";
+}
